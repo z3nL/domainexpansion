eaccba90a76c8aa00d54b317079cfa9df4662a17
Zen Lambertus <zenl@Zens-MacBook-Pro.local>
2025-10-24 23:40:24 -0400

    gradient

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,19 +1,9 @@
-import { useState } from 'react'
-import './App.css'
+import Landing from './Landing'
 
 function App() {
 
   return (
-    <>
-      <h1>Domain Expansion</h1>
-      <div className='landingOptions'>
-        <button className='landingButton'>New Game</button>
-        <button className='landingButton'>Edit Deck</button>
-      </div>
-      <footer className='landingFooter'>
-        <p>by Marrion Forrest, Dillon Wilson, and Zen Lambertus</p>
-      </footer>
-    </>
+    <Landing />
   )
 }
 
